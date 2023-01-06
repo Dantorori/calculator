3bd03c70c8cc0b4f1d54e48800ad5fe8907fe194
Dantorori <jandelyr913@gmail.com>
2023-01-06 05:44:46 +0400

    calculation changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import Display from './components/Display'
 import Opertators from './components/Opertators'
 
 function App() {
-  const [number,setNumber] = useState(0)
+  const actions = ['+',"-","*","/","."]
+  const [number,setNumber] = useState('0')
 
   const addToInput = e => {
     const value = e.target.value
-    if(number === 0) {
+    if(number === "0") {
       setNumber(value)
     }
     else{
@@ -16,15 +17,38 @@ function App() {
     }
   }
 
+  const oper = e => {
+    const value = e.target.value
+    if(actions.includes(number.slice(-1))) {
+      setNumber(number.slice(0,-1)+value)
+    }
+    else {
+      setNumber(number+value)
+    }
+  }
+
+  const calc = () => {
+    setNumber(eval(number).toString())
+  }
+
+  const clearall = () => {
+    setNumber("0")
+  }
+
   const clear = () => {
-    setNumber(0)
+    if (number === "0" || number.length == 1) {
+      setNumber("0")
+    }
+    else {
+      setNumber(number.slice(0,-1))
+    }
   }
 
   return (
       <div className='flex justify-center items-center h-screen bg-zinc-700'>
-        <div className=''>
+        <div className='max-w-[20rem]'>
           <Display value={number}/>
-          <Opertators addToInput={addToInput} clear={clear}/>
+          <Opertators addToInput={addToInput} clearall={clearall} clear={clear} oper={oper} calc={calc}/>
         </div>
       </div>
   )
--- src/components/Opertators.jsx
+++ src/components/Opertators.jsx
@@ -5,14 +5,14 @@ function Opertators(props) {
 
   return (
     <div className='grid gap-3 grid-cols-4 bg-gray-100 p-5 rounded-b-lg'>
-      <button className='icon' onClick={props.clear}>AC</button>
-      <button className='icon'>+/-</button>
+      <button className='icon' onClick={props.clearall}>AC</button>
+      <button className='icon' onClick={props.clear}>C</button>
       <button className='icon'>%</button>
-      <button className='icon col-start-4 row-start-1'>/</button>
-      <button className='icon col-start-4 row-start-2'>*</button>
-      <button className='icon col-start-4 row-start-3'>-</button>
-      <button className='icon col-start-4 row-start-4'>+</button>
-      <button className='icon col-start-3 col-span-2 row-start-5'>=</button>
+      <button value="/" className='icon col-start-4 row-start-1' onClick={props.oper}>/</button>
+      <button value="*" className='icon col-start-4 row-start-2' onClick={props.oper}>*</button>
+      <button value="-" className='icon col-start-4 row-start-3' onClick={props.oper}>-</button>
+      <button value="+" className='icon col-start-4 row-start-4' onClick={props.oper}>+</button>
+      <button className='icon col-start-3 col-span-2 row-start-5' onClick={props.calc}>=</button>
       <button value="9" className='icon' onClick={props.addToInput}>9</button>
       <button value="8" className='icon' onClick={props.addToInput}>8</button>
       <button value="7" className='icon' onClick={props.addToInput}>7</button>
@@ -23,7 +23,7 @@ function Opertators(props) {
       <button value="2" className='icon' onClick={props.addToInput}>2</button>
       <button value="1" className='icon' onClick={props.addToInput}>1</button>
       <button value="0" className='icon' onClick={props.addToInput}>0</button>
-      <button className='icon'>.</button>
+      <button value="." className='icon' onClick={props.oper}>.</button>
     </div>
   )
 }
